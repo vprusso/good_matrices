18a0745635bcb062752225757280d2cd0bdc6b9d
vprusso <vincentrusso1@gmail.com>
2022-06-17 21:47:59 +0000

    Minor changes and cleanup.

---
+++ cleanup.sh
@@ -0,0 +1,11 @@
+rm join_pairedmatchings
+rm generate_matching_instances_comp
+rm generate_pairedmatchings
+rm remove_equivalent_exhaust
+rm remove_equivalent_matchedpairs
+rm -rf matchedpairs
+rm -rf matchedseqns
+rm -rf matchings
+rm -rf timings
+rm -rf tmp
+
