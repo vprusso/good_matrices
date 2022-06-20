73141279a13ab6765094f032bdd678aa207d38b2
vprusso <vincentrusso1@gmail.com>
2022-06-20 18:50:37 -0400

    Adding timing information to driver script.

--- driver.sh
+++ driver.sh
@@ -18,10 +18,35 @@ else
 	make all
 	for n in `seq $1 6 $u`
 	do
+
+		start_time="$(date -u +%s)"
 		./generate_matching_instances_comp $n
+		end_time="$(date -u +%s)"
+		elapsed="$(($end_time-$start_time))"
+		echo "Total of $elapsed seconds elapsed for generate_matching_instances_comp\n"
+
+		start_time="$(date -u +%s)"
 		./generate_pairedmatchings $n
+		end_time="$(date -u +%s)"
+		elapsed="$(($end_time-$start_time))"
+		echo "Total of $elapsed seconds elapsed for generate_pairedmatchings\n"
+
+		start_time="$(date -u +%s)"
 		./sortpairs.sh $n
+		end_time="$(date -u +%s)"
+		elapsed="$(($end_time-$start_time))"
+		echo "Total of $elapsed seconds elapsed for sortpairs\n"
+
+		start_time="$(date -u +%s)"
 		./join_pairedmatchings $n
+		end_time="$(date -u +%s)"
+		elapsed="$(($end_time-$start_time))"
+		echo "Total of $elapsed seconds elapsed for join_pairedmatchings\n"
+
+		start_time="$(date -u +%s)"
 		./remove_equivalent_matchedpairs $n
+		end_time="$(date -u +%s)"
+		elapsed="$(($end_time-$start_time))"
+		echo "Total of $elapsed seconds elapsed for remove_equivalent_matchedpairs\n"
 	done
 fi
